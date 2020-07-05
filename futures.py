7f46474bc8c58b6243b4b966b58f2763bbd37d3d
Dan Ramirez <gavinmarsh100@gmail.com>
2020-07-05 13:10:10 +0100

    created a new futures.py file to test with kafka ubuntu installation

---
+++ futures.py
@@ -0,0 +1,37 @@
+'''
+Copyright (C) 2018-2020  Bryant Moscon - bmoscon@gmail.com
+
+Please see the LICENSE file for the terms and conditions
+associated with this software.
+'''
+from cryptofeed.backends.kafka import TradeKafka, BookKafka
+from cryptofeed import FeedHandler
+from cryptofeed.exchanges import Coinbase
+
+from cryptofeed.defines import TRADES, L2_BOOK
+
+
+"""
+You can run a consumer in the console with the following command
+(assuminng the defaults for the consumer group and bootstrap server)
+
+$ /kafka/bin/kafka-console-consumer --bootstrap-server localhost:9092 --topic trades-COINBASE-BTC-USD
+"""
+
+
+def main():
+    f = FeedHandler()
+    cbs = {TRADES: TradeKafka(), L2_BOOK: BookKafka()}
+
+    f.add_feed(Coinbase(max_depth=10, channels=[TRADES, L2_BOOK], pairs=['BTC-USD'], callbacks=cbs))
+    bitmex_symbols = Bitmex.get_active_symbols()
+    f.add_feed(Bitmex(channels=[OPEN_INTEREST], pairs=['XBTUSD'], callbacks={OPEN_INTEREST: oi}))
+    f.add_feed(Bitmex(channels=[TRADES], pairs=bitmex_symbols, callbacks={TRADES: TradeCallback(trade)}))
+    f.add_feed(Bitmex(pairs=['XBTUSD'], channels=[FUNDING, TRADES], callbacks={FUNDING: FundingCallback(funding), TRADES: TradeCallback(trade)}))
+
+
+    f.run()
+
+
+if __name__ == '__main__':
+    main()
