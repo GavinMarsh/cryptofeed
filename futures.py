e831f387a92d0f312c37adcc87935b84f30688b2
Dan Ramirez <gavinmarsh100@gmail.com>
2020-07-05 13:46:28 +0100

    bug fix

--- futures.py
+++ futures.py
@@ -4,7 +4,7 @@ Copyright (C) 2018-2020  Bryant Moscon - bmoscon@gmail.com
 Please see the LICENSE file for the terms and conditions
 associated with this software.
 '''
-from cryptofeed.backends.kafka import TradeKafka, BookKafka
+from cryptofeed.backends.kafka import TradeKafka, BookKafka, FundingKafka, BookDeltaKafka, TickerKafka, OpenInterestKafka
 from cryptofeed import FeedHandler
 from cryptofeed.exchanges import Coinbase, Bitmex
 
