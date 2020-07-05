1723232d286f45f7c92beda837c4debf62b606c1
Dan Ramirez <gavinmarsh100@gmail.com>
2020-07-05 14:08:28 +0100

    bug fix

--- bitmexProducer.py
+++ bitmexProducer.py
@@ -23,7 +23,6 @@ def main():
     f = FeedHandler()
     cbs = {TRADES: TradeKafka(), L2_BOOK: BookKafka(), OPEN_INTEREST: OpenInterestKafka()}
 
-    f.add_feed(Coinbase(max_depth=10, channels=[TRADES, L2_BOOK], pairs=['BTC-USD'], callbacks=cbs))
     bitmex_symbols = Bitmex.get_active_symbols()
     f.add_feed(Bitmex(channels=[OPEN_INTEREST], pairs=['XBTUSD'], callbacks=cbs))
     f.add_feed(Bitmex(channels=[TRADES], pairs=bitmex_symbols, callbacks=cbs))
