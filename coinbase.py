65495f8bcacf57876232edd83645a6d5642c757b
Dan Ramirez <gavinmarsh100@gmail.com>
2020-07-06 13:25:06 +0100

    code update

--- coinbase.py
+++ coinbase.py
@@ -8,7 +8,7 @@ from cryptofeed.backends.kafka import TradeKafka, BookKafka, FundingKafka, BookD
 from cryptofeed import FeedHandler
 from cryptofeed.exchanges import Coinbase
 
-from cryptofeed.defines import TRADES, L2_BOOK, OPEN_INTEREST, FUNDING, COINBASE, TICKER, TickerCallback
+from cryptofeed.defines import TRADES, L2_BOOK, OPEN_INTEREST, FUNDING, COINBASE, TICKER,
 
 
 """
@@ -21,11 +21,11 @@ $ ~/kafka/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topi
 
 def main():
     f = FeedHandler()
-    cbs = {TRADES: TradeKafka(), L2_BOOK: BookKafka(), OPEN_INTEREST: OpenInterestKafka()}
+    cbs = {TRADES: TradeKafka(), L2_BOOK: BookKafka(), OPEN_INTEREST: OpenInterestKafka(), TickerKafka()}
 
-    f.add_feed(COINBASE, pairs=['BTC-USD'], channels=[TICKER], callbacks={TICKER: TickerCallback(ticker)})
-    f.add_feed(Coinbase(pairs=['BTC-USD'], channels=[TRADES], callbacks={TRADES: TradeCallback(trade)}))
-    f.add_feed(Coinbase(config={L2_BOOK: ['BTC-USD', 'ETH-USD'], TRADES: ['ETH-USD', 'BTC-USD']}, callbacks={TRADES: TradeCallback(trade), L2_BOOK: BookCallback(book)}))
+    f.add_feed(COINBASE, pairs=['BTC-USD'], channels=[TICKER], callbacks={TICKER: TickerKafka(ticker))
+    f.add_feed(Coinbase(pairs=['BTC-USD'], channels=[TRADES], callbacks={TRADES: TradeKafka(trade)}))
+    f.add_feed(Coinbase(config={L2_BOOK: ['BTC-USD'], TRADES: ['BTC-USD']}, callbacks={TRADES: Tradekafka(trade), L2_BOOK: Bookkafka(book)}))
 
 
     f.run()
