106d6b1b9c6a26cb229793435d03e977bbc6d71a
Dan Ramirez <gavinmarsh100@gmail.com>
2020-07-07 16:47:39 +0100

    adding exchange producers

--- binance.py
+++ binance.py
@@ -10,7 +10,7 @@ from cryptofeed.defines import TICKER, TRADES, L2_BOOK
 def main():
     f = FeedHandler()
 
-    f.add_feed(Binance(pairs=['BTC-USDT'], channels=[TRADES, TICKER, L2_BOOK], callbacks={L2_BOOK: BookKafka(), TRADES: TradeKafka(), TICKER: TickerKafka()}))
+    f.add_feed(BinanceFutures(pairs=['BTC-USDT-'], channels=[TRADES, TICKER, L2_BOOK], callbacks={L2_BOOK: BookKafka(), TRADES: TradeKafka(), TICKER: TickerKafka()}))
 #   f.add_feed(Binance(pairs=['BTC-USDT'], channels=[TRADES, TICKER, L2_BOOK], callbacks={L2_BOOK: BookKafka(), TRADES: TradeKafka(), TICKER: TickerKafka()}))
 
 #    f.add_feed(Huobi(config=config, callbacks={TRADES: TradeKafka(), L2_BOOK: BookKafka()}))
