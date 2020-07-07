4b1c0f5f2adbf09ea90a16bf94f8c4e69bb48434
Dan Ramirez <gavinmarsh100@gmail.com>
2020-07-07 17:54:17 +0100

    adding exchange producers

--- binance.py
+++ binance.py
@@ -3,9 +3,9 @@ Copyright (C) 2018-2020
 '''
 from cryptofeed.backends.kafka import TickerKafka, TradeKafka, BookKafka
 from cryptofeed import FeedHandler
-from cryptofeed.exchanges import BinanceFutures
-#from cryptofeed.defines import TICKER, TRADES, L2_BOOK
-from cryptofeed.defines import TICKER, TRADES, L2_BOOK, BINANCE_FUTURES
+from cryptofeed.exchanges import Binance
+#from cryptofeed.exchanges import BinanceFutures #tried useing BinanceFutures but symbol is not supprted on exchange
+from cryptofeed.defines import TICKER, TRADES, L2_BOOK
 
 
 '''
@@ -14,8 +14,8 @@ currently unable to pull futures ticker data?
 def main():
     f = FeedHandler()
 
-    f.add_feed(BinanceFutures(pairs=['BTC-USDT'], channels=[TRADES, TICKER, L2_BOOK], callbacks={L2_BOOK: BookKafka(), TRADES: TradeKafka(), TICKER: TickerKafka()}))
-#    f.add_feed(Binance(pairs=['BTC-USDT'], channels=[TRADES, TICKER, L2_BOOK], callbacks={L2_BOOK: BookKafka(), TRADES: TradeKafka(), TICKER: TickerKafka()}))
+#    f.add_feed(BinanceFutures(pairs=['BTC-USDT'], channels=[TRADES, TICKER, L2_BOOK], callbacks={L2_BOOK: BookKafka(), TRADES: TradeKafka(), TICKER: TickerKafka()})) # uncomment to test BinanceFutures, correct code
+    f.add_feed(Binance(pairs=['BTC-USDT'], channels=[TRADES, TICKER, L2_BOOK], callbacks={L2_BOOK: BookKafka(), TRADES: TradeKafka(), TICKER: TickerKafka()}))
 
 
 
