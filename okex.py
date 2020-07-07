f0c614d71307d0fea267f0fdb37acb76b783ab08
Dan Ramirez <gavinmarsh100@gmail.com>
2020-07-07 12:37:51 +0100

    adding exchange producers

--- okex.py
+++ okex.py
@@ -1,33 +1,21 @@
 '''
-Copyright (C) 2018-2020  Bryant Moscon - bmoscon@gmail.com
-
-Please see the LICENSE file for the terms and conditions
-associated with this software.
+Copyright (C) 2018-2020
 '''
 from cryptofeed.backends.kafka import TradeKafka, BookKafka, FundingKafka, BookDeltaKafka, TickerKafka, OpenInterestKafka
 from cryptofeed import FeedHandler
 from cryptofeed.exchanges import OKEx, OKCoin
 from cryptofeed.defines import TRADES, L2_BOOK, OPEN_INTEREST, FUNDING, TRADES_FUTURES
 
-"""
-You can run a consumer in the console with the following command
-(assuminng the defaults for the consumer group and bootstrap server)
-
-$ ~/kafka/bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic trades-COINBASE-BTC-USD
-"""
-
 def main():
     f = FeedHandler()
 
     okex_symbols = OKEx.get_active_symbols()
-#    f.add_feed(OKEx(pairs=okex_symbols, channels=[TRADES], callbacks={TRADES: TradeKafka()}))
-
-    f.add_feed(OKEx(pairs=okex_symbols, channels=[TRADES_FUTURES], callbacks={TRADES_FUTURES: TradeKafka()}))
+    f.add_feed(OKEx(pairs=okex_symbols, channels=[TICKER_FUTURES], callbacks={TICKER_FUTURES: TradeKafka()}))
+    f.add_feed(OKEx(pairs=['BTC-USDT'], channels=[TICKER], callbacks={TICKER: TickerKafka()}))
     f.add_feed(OKEx(pairs=['BTC-USDT'], channels=[TRADES], callbacks={TRADES: TradeKafka()}))
     f.add_feed(OKCoin(pairs=['BTC-USD'], channels=[L2_BOOK], callbacks={L2_BOOK: BookKafka()}))
 
     f.run()
-
 
 if __name__ == '__main__':
     main()
