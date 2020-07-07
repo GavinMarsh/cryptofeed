9f52873e6be0f3ac668dcbe8ae0364c334495424
Dan Ramirez <gavinmarsh100@gmail.com>
2020-07-07 12:44:55 +0100

    adding exchange producers

--- okex.py
+++ okex.py
@@ -1,17 +1,17 @@
 '''
 Copyright (C) 2018-2020
 '''
-from cryptofeed.backends.kafka import TradeKafka, BookKafka, FundingKafka, BookDeltaKafka, TickerKafka, OpenInterestKafka
+from cryptofeed.backends.kafka import TickerKafka, TradeKafka, BookKafka
 from cryptofeed import FeedHandler
 from cryptofeed.exchanges import OKEx, OKCoin
-from cryptofeed.defines import TRADES, L2_BOOK, OPEN_INTEREST, FUNDING, TICKER_FUTURES, TRADES_FUTURES, TICKER
+from cryptofeed.defines import TICKER, TICKER_FUTURES, TRADES, TRADES_FUTURES, L2_BOOK
 
 def main():
     f = FeedHandler()
 
     okex_symbols = OKEx.get_active_symbols()
     f.add_feed(OKEx(pairs=okex_symbols, channels=[TICKER_FUTURES], callbacks={TICKER_FUTURES: TradeKafka()}))
-    f.add_feed(OKEx(pairs=['BTC-USDT'], channels=[TICKER], callbacks={TICKER: TickerKafka()}))
+#    f.add_feed(OKEx(pairs=['BTC-USDT'], channels=[TICKER], callbacks={TICKER: TickerKafka()}))
     f.add_feed(OKEx(pairs=['BTC-USDT'], channels=[TRADES], callbacks={TRADES: TradeKafka()}))
     f.add_feed(OKCoin(pairs=['BTC-USD'], channels=[L2_BOOK], callbacks={L2_BOOK: BookKafka()}))
 
