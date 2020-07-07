4e79efd5f97dbb8036f92a260b8af28457e481eb
Dan Ramirez <gavinmarsh100@gmail.com>
2020-07-07 12:47:06 +0100

    adding exchange producers

--- okex.py
+++ okex.py
@@ -10,7 +10,8 @@ def main():
     f = FeedHandler()
 
     okex_symbols = OKEx.get_active_symbols()
-    f.add_feed(OKEx(pairs=okex_symbols, channels=[TICKER_FUTURES], callbacks={TICKER_FUTURES: TradeKafka()}))
+#    f.add_feed(OKEx(pairs=okex_symbols, channels=[TICKER_FUTURES], callbacks={TICKER_FUTURES: TradeKafka()}))
+    f.add_feed(OKEx(pairs=okex_symbols, channels=[TRADES_FUTURES], callbacks={TRADES_FUTURES: TradeKafka()}))
 #    f.add_feed(OKEx(pairs=['BTC-USDT'], channels=[TICKER], callbacks={TICKER: TickerKafka()}))
     f.add_feed(OKEx(pairs=['BTC-USDT'], channels=[TRADES], callbacks={TRADES: TradeKafka()}))
     f.add_feed(OKCoin(pairs=['BTC-USD'], channels=[L2_BOOK], callbacks={L2_BOOK: BookKafka()}))
