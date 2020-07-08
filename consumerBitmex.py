5c7ef958332942dd99593b98d0823f111ca0efbe
Dan Ramirez <gavinmarsh100@gmail.com>
2020-07-08 11:55:44 +0100

    addition

--- consumerBitmex.py
+++ consumerBitmex.py
@@ -4,7 +4,8 @@ Copyright (C) 2017-2020  Rez
 from cryptofeed.backends.kafkahandler import connect_kafka_consumer
 from kafka import KafkaConsumer
 
-consumer = KafkaConsumer('trades-BITMEX-XBTUSD')
+#consumer = KafkaConsumer('trades-BITMEX-XBTUSD')
+consumer = connect_kafka_consumer('trades-BITMEX-XBTUSD')
 for msg in consumer:
     print (msg)
 
--- cryptofeed/backends/kafkahandler.py
+++ cryptofeed/backends/kafkahandler.py
@@ -6,7 +6,7 @@ Copyright (C) 2017-2020  Rez
 def connect_kafka_consumer(mytopic):
     _consumer = None
     try:
-        _consumer = KafkaConsumer(bootstrap_servers=['localhost:9092'],topic=topic)
+        _consumer = KafkaConsumer(bootstrap_servers=['localhost:9092'],topic=mytopic)
     except Exception as ex:
         print('Exception while connecting Kafka Consumer')
         print(str(ex))
