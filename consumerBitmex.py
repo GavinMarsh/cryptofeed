40320e5c6a9e5a3424a73f4cc2d785fcecd8858f
Dan Ramirez <gavinmarsh100@gmail.com>
2020-07-08 12:01:56 +0100

    addition

--- consumerBitmex.py
+++ consumerBitmex.py
@@ -4,9 +4,8 @@ Copyright (C) 2017-2020  Rez
 from cryptofeed.backends.kafkahandler import connect_kafka_consumer
 from kafka import KafkaConsumer
 
-#consumer = KafkaConsumer('trades-BITMEX-XBTUSD')
-consumer = connect_kafka_consumer('trades-BITMEX-XBTUSD')
-for msg[1] in consumer:
+consumer = KafkaConsumer('trades-BITMEX-XBTUSD')
+for msg in consumer:
     print (msg)
 
 #if __name__ == '__main__':
