ef10ef91cba1563b95744bd164051be5903b11fc
Dan Ramirez <gavinmarsh100@gmail.com>
2020-07-08 11:57:56 +0100

    addition

--- consumerBitmex.py
+++ consumerBitmex.py
@@ -6,8 +6,8 @@ from kafka import KafkaConsumer
 
 #consumer = KafkaConsumer('trades-BITMEX-XBTUSD')
 consumer = connect_kafka_consumer('trades-BITMEX-XBTUSD')
-for msg[1] in consumer:
-    print (msg)
+#for msg[1] in consumer:
+    print (msg[1])
 
 if __name__ == '__main__':
     main()
