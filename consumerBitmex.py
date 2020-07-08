48c58f5fa8d601282cb7f04689c5ee7f080831fc
Dan Ramirez <gavinmarsh100@gmail.com>
2020-07-08 12:15:49 +0100

    addition

--- consumerBitmex.py
+++ consumerBitmex.py
@@ -4,7 +4,8 @@ Copyright (C) 2017-2020  Rez
 from cryptofeed.backends.kafkahandler import connect_kafka_consumer
 from kafka import KafkaConsumer
 
-consumer = KafkaConsumer('trades-BITMEX-XBTUSD')
+consumer = KafkaConsumer('trades-BITMEX-XBTUSD',bootstrap_servers = ['localhost:9092'],
+value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 for message in consumer:
     print (message[6]['price'])
 
