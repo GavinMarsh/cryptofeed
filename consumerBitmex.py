c5be5c3208a3ec54a457dd357d439eb656573f44
Dan Ramirez <gavinmarsh100@gmail.com>
2020-07-08 12:19:51 +0100

    addition

--- consumerBitmex.py
+++ consumerBitmex.py
@@ -8,9 +8,12 @@ import json
 consumer = KafkaConsumer('trades-BITMEX-XBTUSD',bootstrap_servers = ['localhost:9092'],
 value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 for message in consumer:
-    print (message[6]['price'])
+    print (message[6]["Spot price: ",'price'])
 
-
+consumer2 = KafkaConsumer('trades-BITMEX-XBTZ20',bootstrap_servers = ['localhost:9092'],
+value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+for message2 in consumer2:
+    print (message2[6]["Dec Futures price: ",'price'])
 
 
 
